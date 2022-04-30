1f5d27e1c6d79969397a7b235f4fd9223b22b0b8
divyameenakshi24 <75235402+divyameenakshi24@users.noreply.github.com>
2022-04-30 22:58:41 +0530

    Add files via upload

---
+++ DIP exp 5 (1).py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+original_img=cv2.imread("bts.jpg")
+original_img=cv2.cvtColor(original_img,cv2.COLOR_BGR2RGB)
+plt.axis("off")
+plt.imshow(original_img)
+row,col,dim=original_img.shape
+
+
+# In[3]:
+
+
+Translation_matrix=np.float32([[1,0,120],[0,1,120],[0,0,1]])
+Translated_image=cv2.warpPerspective(original_img,Translation_matrix,(col,row))
+plt.axis("off")
+plt.imshow(Translated_image)
+
+
+# In[4]:
+
+
+Scaling_Matrix=np.float32([[1.2,0,0],[0,1.2,0],[0,0,1]])
+Scaled_image=cv2.warpPerspective(original_img,Scaling_Matrix,(col,row))
+plt.axis("off")
+plt.imshow(Scaled_image)
+
+
+# In[5]:
+
+
+Shearing_matrix=np.float32([[1,0.2,0],[0.2,1,0],[0,0,1]])
+Sheared_image=cv2.warpPerspective(original_img,Shearing_matrix,(col*2,int(row*1.5)))
+plt.axis("off")
+plt.imshow(Sheared_image)
+
+
+# In[6]:
+
+
+Reflection_matrix_col=np.float32([[-1,0,col],[0,1,0],[0,0,1]])
+Reflected_image_col=cv2.warpPerspective(original_img,Reflection_matrix_col,(col,int(row)))
+plt.axis("off")
+plt.imshow(Reflected_image_col)
+
+
+# In[7]:
+
+
+Reflection_matrix_row=np.float32([[1,0,0],[0,-1,row],[0,0,1]])
+Reflected_image_row=cv2.warpPerspective(original_img,Reflection_matrix_row,(col,int(row)))
+plt.axis("off")
+plt.imshow(Reflected_image_row)
+
+
+# In[8]:
+
+
+Rotation_angle=np.radians(10)
+Rotation_matrix=np.float32([[np.cos(Rotation_angle),-np.sin(Rotation_angle),0],
+                                [np.sin(Rotation_angle),np.cos(Rotation_angle),0],
+                                [0,0,1]])
+Rotated_image=cv2.warpPerspective(original_img,Rotation_matrix,(col,(row)))
+plt.axis("off")
+plt.imshow(Rotated_image)
+
+
+# In[10]:
+
+
+cropped_image=original_img[10:350,320:560]
+plt.axis("off")
+plt.imshow(cropped_image)
+
+
+# In[ ]:
+
+
+
+
